42802709a76527415974bf83f0ccf2a7e4cdd05d
Alejandro Osornio <aoxo.contact@gmail.com>
2024-10-24 08:34:24 -0600

    Clase

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { getLatestGames } from './lib/metacritic';
 
 export default function App() {
+  const [games, set_games] = useState([])
+
+  // useEffect carga todo cuando se refresca el componente
+  useEffect(() => {
+    getLatestGames().then((games) => {
+      set_games(games);
+    })
+  }, 
+  []);
+
+  // <Text>Open up App.js to start working on your app</Text>
+  // <Text>{JSON.stringify(games[0])}</Text>
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ScrollView style={{padding: 20}} contentContainerStyle={{paddingVertical: 25,rowGap: 20}}>
+        {games.map((game) => (
+          <View key={game.slug} style={{padding: 20, width: 'auto', backgroundColor: '#f5f5f5', borderRadius: 20}}>
+            <View flexDirection='col'>
+              <Image style={styles.image} source={{uri: game.image}}></Image>
+              <View style={{paddingTop: 10}}>
+                <View flexDirection='row'>
+                  <Text style={styles.score}>{game.score}</Text>
+                  <Text style={styles.title}>{game.title}</Text>
+                </View>
+                <Text style={styles.description}>{game.description}</Text>
+              </View>
+            </View>
+          </View>
+        ))}
+      </ScrollView>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +47,29 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    flexWrap: 'wrap',
   },
+  image: {
+    width: '100%',
+    height: 100,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+    flexWrap: 'wrap',
+    flex: 1,
+  },
+  score: {
+    fontSize: 20,
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+    color: 'green',
+    paddingRight: 5,
+  },
+  description: {
+    paddingTop: 10,
+    fontSize: 14,
+  }
 });
